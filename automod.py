483de136a6d3358663d2cb3909a99b69aaf90068
ma-xiii <20654098+ma-xiii@users.noreply.github.com>
2021-08-07 16:05:18 +0800

    Update automod.py

--- automod.py
+++ automod.py
@@ -1,73 +1,62 @@
-import json
 import discord
 from discord.ext import commands
 import asyncio
-from replit import db
-
+from datetime import datetime
+from pymongo import MongoClient
 spam_detect = []
+gif_detect = []
 
-with open("data.json", "r") as datab:
-    config = json.load(datab)
-    logchannel = config["log_channel"]
-    personal_blacklist = config["personal_blacklist"]
-    regular_blacklist = config["regular_blacklist"]
-    bot_channels = config["bot_channels"]
-
-class autmod(commands.Cog):
+class automod(commands.Cog):
     def __init__(self, client):
         self.client = client
-    
+        self.cluster = MongoClient("mongodb+srv://maxcodez:zbndxaPz1BNseBgl@cluster0.hqubn.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        self.db = self.cluster["discord"]
+        self.info = self.db["info"]
+        self.moderation = self.db["moderation"]
+        self.bots = self.info.find_one({"name":"bots"})["value"]
+        self.bot_channel = (self.info.find_one({"name":"botchannel"})["value"])
+        self.norank = self.info.find_one({"name":"bots"})["value"]
+        self.regular_blacklist = self.info.find_one({"name":"swear"})["value"]
+        self.personal_blacklist = self.info.find_one({"name":"blacklist"})["value"]
+        self.logchannel = self.info.find_one({"name":"log"})["value"]
 
-    '''
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
-        guild = discord.utils.find(lambda g : g.id == payload.guild_id, self.client.guilds)
-        author = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-        
-        guild = discord.utils.find(lambda g : g.id == payload.guild_id, self.client.guilds)
-        if payload.emoji.name == "😤":
-            channelsend = discord.utils.get(guild.channels, id=payload.channel_id)
-            message = await channelsend.fetch_message(payload.message_id)
-            muterole = discord.utils.get(guild.roles, name="mute")
-            reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
-            ismuted = False
-            if muterole in message.author.roles:
-                ismuted = True
-
-            if (reaction.count > 1) and (ismuted == False):
-                await channelsend.send(f"https://discord.com/channels/719103069828284478/{channelsend.id}/{message.id}")
-                await message.author.add_roles(muterole)
-                await channelsend.send(f"{message.author.mention} was muted... until unmuted?")
-      '''
             
 
-    @commands.command()
-    @commands.has_any_role("MaxCodez")
-    async def clearmute(self, ctx, member: discord.Member, *, reason=None): 
-        keys = db.keys()
-        for key in keys:
-            print(key)
-            del key
     @commands.Cog.listener()
     async def on_ready(self):
+        guild = self.client.get_guild(799977201898487818)
+        channelsend = discord.utils.get(guild.channels, id=self.logchannel)
+        self.gif_counter(self)
         while True:
+            time = datetime.now().strftime('%H:%M')
             await asyncio.sleep(10)
             spam_detect.clear()
+            gif_detect.clear()
+            info = self.moderation.find_one({"time":time})
+            if info != None:
+                self.moderation.delete_one({"time":time})
+                member = discord.utils.find(lambda m : m.id == int(info["mute"]), guild.members)
+                await member.remove_roles(discord.utils.get(guild.roles, name="Detention"))
+                embed = discord.Embed(description=f"**{member.name}** was unmuted", color=0x4DC8E9)
+                await channelsend.send(embed=embed)
+
+            
+            
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        for i in range(len(regular_blacklist)):
-            if regular_blacklist[i] in str(after.content).lower():
+        for i in range(len(self.regular_blacklist)):
+            if self.regular_blacklist[i] in str(after.content).lower():
                 await after.delete()
         
-        for i in range(len(personal_blacklist)):
-            if personal_blacklist[i] in str(after.content).lower():
+        for i in range(len(self.personal_blacklist)):
+            if self.personal_blacklist[i] in str(after.content).lower():
                 await after.delete()
                 await after.author.guild.ban(after.author, reason="blacklisted word")
-                modlog = discord.utils.get(after.guild.channels, id=int(logchannel))
+                modlog = discord.utils.get(after.guild.channels, id=self.logchannel)
                 embed = discord.Embed(
-                    description="{0} banned for using blacklisted word: {1}".format(after.author.mention,personal_blacklist[i]),
-                    color=0xdddd1e)
+                    description="{0} banned for using blacklisted word: {1}".format(after.author.mention,self.personal_blacklist[i]),
+                    color=0x4DC8E9)
                 await modlog.send(embed=embed)
         
         if (("@everyone" in after.content) or ("@here" in after.content)) and (after.author.id != 359252304576380931):
@@ -76,54 +65,90 @@ class autmod(commands.Cog):
         if "<@!359252304576380931>" in after.content or "@MaxCodez" in after.content:
             await after.delete()
 
+    
+
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        for i in range(len(personal_blacklist)):
-            if personal_blacklist[i] in str(message.content).lower():
+        for i in range(len(self.personal_blacklist)):
+            if self.personal_blacklist[i] in str(message.content).lower():
                 await message.delete()
                 await message.author.guild.ban(message.author, reason="blacklisted word")
-                modlog = discord.utils.get(message.guild.channels, id=int(logchannel))
+                modlog = discord.utils.get(message.guild.channels, id=int(self.logchannel))
                 embed = discord.Embed(
-                    description="{0} banned for using blacklisted word: {1}".format(message.author.mention,personal_blacklist[i]),
-                    color=0xdddd1e)
+                    description="{0} banned for using blacklisted word: {1}".format(message.author.mention,self.personal_blacklist[i]),
+                    color=0x4DC8E9)
                 await modlog.send(embed=embed)
-        for i in range(len(regular_blacklist)):
-            if regular_blacklist[i] in str(message.content).lower():
+        for i in range(len(self.regular_blacklist)):
+            if self.regular_blacklist[i] in str(message.content).lower():
                 await message.delete()
 
-        if (("@everyone" in message.content) or ("@here" in message.content)) and (message.author.id != 359252304576380931):
+        if (("@everyone" in message.content) or ("@here" in message.content)) and (message.author.id not in self.bots):
             await message.delete()
 
-        if "<@!359252304576380931>" in message.content or "@MaxCodez" in message.content:
+        if ("<@!359252304576380931>" in message.content or "@MaxCodez" in message.content) and (message.author.id not in self.bots):
             await message.delete()
             embed = discord.Embed(
-                description="{0} HAHA didn't you see rule 9? If you need help ping a code helper in <#719103243459756052>".format(
-                    message.author.mention), color=0xdddd1e)
+                description="Max is a busy guy! If he isn't already here it's probably for a good reason!".format(message.author.mention), color=0x4DC8E9)
             await message.channel.send(embed=embed)
-
+        '''
+        if "https" in message.content:
+            gif_detect.append(message.author.id)
+        gif_counter = 0
+        for i in range(len(gif_detect)):
+            if gif_detect[i] == (message.author.id):
+                gif_counter += 1
+      
         counter = 0
         for i in range(len(spam_detect)):
             if spam_detect[i] == str(message.author.id):
                 counter += 1
         spam_detect.append(str(message.author.id))
-        if (counter > 5) and (message.channel.id not in bot_channels):
-            print(spam_detect)
-            await message.author.send("you have been kicked for spamming too much")
-            await message.guild.ban(message.author, reason="spam")
-            await asyncio.sleep(1)
-            await message.guild.unban(message.author)
-            spam_detect.clear()
-            modlog = discord.utils.get(message.guild.channels, id=int(logchannel))
-            embed = discord.Embed(description="{} has been kicked after spamming".format(message.author.mention), color=0xdddd1e)
-            await modlog.send(embed=embed)
+
+        if (counter > 7) or (gif_counter > 1):
+                print(spam_detect)
+                await message.author.send("you have been kicked for spamming too much")
+                await message.guild.ban(message.author, reason="spam")
+                await asyncio.sleep(1)
+                await message.guild.unban(message.author)
+                spam_detect.clear()
+                modlog = discord.utils.get(message.guild.channels, id=int(self.logchannel))
+                embed = discord.Embed(description="{} has been kicked after spamming".format(message.author.mention), color=0x4DC8E9)
+                await modlog.send(embed=embed)
+        '''
+    
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        modlog = discord.utils.get(message.guild.channels, id=int(self.logchannel))
+        embed = discord.Embed(title="Message deleted", color=0x7a869b)
+        embed.add_field(name="Author: ", value=f"{message.author.mention}")
+        embed.add_field(name="Channel: ", value=f"{message.channel}")
+        embed.add_field(name="Message: ", value=message.content, inline=False)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        await modlog.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        modlog = discord.utils.get(before.guild.channels, id=int(self.logchannel))
+        embed = discord.Embed(title="Message edited", color=0x4DC8E9)
+        embed.add_field(name="Author: ", value=f"{before.author.mention}")
+        embed.add_field(name="Channel: ", value=f"{before.channel}")
+        embed.add_field(name="Before: ", value=before.content, inline=False)
+        embed.add_field(name="After: ", value=after.content, inline=False)
+        embed.set_thumbnail(url=before.author.avatar_url)
+        await modlog.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        modlog = discord.utils.get(member.guild.channels, id=int(self.logchannel))
+        embed = discord.Embed(title="Member left :(", color=0x4DC8E9)
+        embed.add_field(name="Member: ", value=member.mention, inline=True)
+        embed.add_field(name="Left: ", value=member.guild.member_count, inline=True)
+        embed.set_thumbnail(url=member.avatar_url)
+        await modlog.send(embed=embed)
+
+    
 
 def setup(client):
-    client.add_cog(autmod(client))
-    '''
-    @commands.command()
-    @commands.has_any_role("Python helper", "Moderator", "admin", "MaxCodez")
-    async def afk(self, ctx, *args):
-        msg = (' '.join(args))
-        db[str(ctx.author.id)] = msg
-        await ctx.channel.send("AFK set")
-    '''
+    client.add_cog(automod(client))
+
